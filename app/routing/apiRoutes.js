350a230739404a433bce1082bb5d25874c87a546
Melanie West <dhmofan@gmail.com>
2018-05-02 17:07:39 -0400

    paths are now set up and partially working in the routing files.  Even the modal popped up!  No info inside it yet, but that's a first.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,67 @@
 var express = require('express');
-
 var apifriends = express.Router();
 
-apifriends.get('/', function(req, res) {
-  res.send('Hello from API friends root route.');
+//data from other files
+var friendsArray = require('../data/friends.js')
+
+//access the data and display
+
+apifriends.get('/list', function (req, res) {
+  res.json(friendsArray);
 });
- 
-apifriends.get('/list', function(req, res) {
-  res.send('List of friends.');
+
+// apifriends.get('/list', function(req, res) {
+//   res.send('List of friends.');
+// });
+
+
+
+apifriends.post('/', function (req, res) {
+  res.send('Hello from API friends root route.');
+
+  var friendName = req.body.name;
+  var friendPhoto = req.body.photo;
+  var friendScores = req.body.scores;
+
+  var newFriend = {
+    name: friendName,
+    scores: friendScores,
+    photo: friendPhoto
+  }
+
+  // friendsArray.push(newFriend)
+
+  // res.json(newFriend)	
+
+  function findMatch(data) {
+    var lowestDiff = 100;
+    var match;
+
+    for (var i = 0; i < friendsArray.length; i++) {
+
+      var currentDiff = 0;
+
+      for (var j = 0; j < newFriend.scores.length; j++) {
+        //+= means to take the current value and add whatevers to the left of =...
+        //math. ab-->give absoulute number so i can do (ex. 1-2), and get a positive answer
+        //parseInt-->takes string makes it an integer
+        currentDiff += Math.abs(parseInt(newFriend.scores[j]) - parseInt(friendsArray[i].scores[j]));
+      }
+      // console.log(currentDiff);
+      //compare current diff to the lowest diff, if it's lower, set that as the new match.
+      if (currentDiff <= lowestDiff) {
+        lowestDiff = currentDiff;
+        match = friendsArray[i];
+        console.log(match)
+
+      }
+    }
+    return match;
+    res.json(match);
+  }
+  findMatch();
+
+
 });
 
 module.exports = apifriends;
--- server.js
+++ server.js
@@ -4,10 +4,6 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var fs = require('fs')
 
-
-//data from other files
-var friendsArray = require('./app/data/friends.js')
-
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -35,64 +31,8 @@ app.get('/', function(req, res) {
 
 
 
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "app/public/home.html"));
-// });
 
-// app.get("/survey", function(req, res) {
-//   res.sendFile(path.join(__dirname, "app/public/survey.html"));
-// });
 
-// app.get('/api/friends', function(req, res){
-//   res.json(friendsArray);
-// });
-
-// app.post('/api/friends', function(req, res){
-
-// 	var friendName = req.body.name;
-// 	var friendPhoto = req.body.photo;
-// 	var friendScores = req.body.scores;
-	
-// 	var newFriend = {
-// 		name : friendName,
-// 		scores : friendScores,
-// 		photo : friendPhoto
-// 	}
-
-// 	// friendsArray.push(newFriend)
-	
-// 	// res.json(newFriend)	
-
-// 	function findMatch(data){
-//     var lowestDiff = 100;
-//     var match;
-    
-//     for (var i = 0; i < friendsArray.length; i++){
-     
-//         var currentDiff = 0;
-       
-//         for (var j = 0; j < newFriend.scores.length; j++){
-//        //+= means to take the current value and add whatevers to the left of =...
-//        //math. ab-->give absoulute number so i can do (ex. 1-2), and get a positive answer
-//        //parseInt-->takes string makes it an integer
-//             currentDiff+=Math.abs(parseInt(newFriend.scores[j]) - parseInt(friendsArray[i].scores[j]));
-//         }
-//         // console.log(currentDiff);
-//         //compare current diff to the lowest diff, if it's lower, set that as the new match.
-//         if (currentDiff <= lowestDiff){
-//             lowestDiff = currentDiff;
-//             match = friendsArray[i];
-//             console.log(match) 
-
-//         }
-//     }
-//     return match;
-//       // res.json(match);
-// }
-// findMatch();
-// });
-
- 
 
 //lets the server recongnize the js files
 app.use(express.static('app'));
